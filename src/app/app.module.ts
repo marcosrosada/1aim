b643e32a93b5d712f5d54331dc0da2a54d10ffe7
Marcos Rosada <ochild@gmail.com>
2017-08-12 18:33:32 -0300

    Header config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import 'materialize-css';
+import { MaterializeModule } from 'angular2-materialize';
+
+import { AppRouting } from './app.routing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterializeModule,
+    AppRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,19 @@
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+const appRouter: Routes = [
+    { path: '', component: HomeComponent }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRouter),
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRouting {}
